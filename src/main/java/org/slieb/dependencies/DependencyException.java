1f09bfb2eec82766756daa1ca67e1fbc27181b07
Stefan Liebenberg <siga.fredo@gmail.com>
2015-04-13 23:44:36 +0200

    improving circular exception

--- src/main/java/org/slieb/dependencies/DependencyException.java
+++ src/main/java/org/slieb/dependencies/DependencyException.java
@@ -23,12 +23,20 @@ public class DependencyException extends RuntimeException {
         super(s, throwable, b, b1);
     }
 
-
     public static DependencyException nothingProvides(String missingProvide) {
         throw new DependencyException(String.format("nothing provides %s", missingProvide));
     }
 
     public static DependencyException circularError(String ns, Set<? extends DependencyNode> parents) {
-        throw new DependencyException();
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Circular dependency detected while trying to resolve \"%s\".\n", ns));
+        builder.append("  Parents: \n");
+        for (DependencyNode<?> node : parents) {
+            builder.append("    + ").append(node.getResource()).append("\n");
+            for (String provide : node.getProvides()) {
+                builder.append("        \"").append(provide).append("\"\n");
+            }
+        }
+        throw new DependencyException(builder.toString());
     }
 }
